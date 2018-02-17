46ec3281cdb39a8999afc7a936548d82134cba5b
Eric Oeur <ericeoeur@gmail.com>
2018-02-16 18:06:37 -0600

    This is the final update for the burger app

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,7 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
+
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -23,9 +19,8 @@ function objToSql(ob) {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
+    
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
@@ -41,29 +36,21 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
+  all: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
   },
-  insertOne: function(burgerName, cb) {
-    /*var queryString = "INSERT INTO pizzas";
-    queryString += "(pizza_name, devoured)";
-    queryString += " VALUES"
-    queryString += "(";
-    queryString += pizzaName + ",";
-    queryString += false
-    queryString += ") ";
-    console.log(queryString);*/
+  insertOne: function (burgerName, cb) {
 
     connection.query('INSERT INTO burgers SET ?', {
-        burger_name: burgerName,
-        devoured: false
-      }, function(err, result) {
+      burger_name: burgerName,
+      devoured: false
+    }, function (err, result) {
       if (err) {
         throw err;
       }
@@ -71,15 +58,14 @@ var orm = {
       cb(result);
     });
   },
-  update: function(burgerID, cb) {
+  update: function (burgerID, cb) {
     var queryString = "UPDATE burgers";
-
     queryString += " SET ";
-	queryString += "devoured = " + true;
-	queryString += " WHERE ";
-	queryString += "id = " + burgerID;
+    queryString += "devoured = " + true;
+    queryString += " WHERE ";
+    queryString += "id = " + burgerID;
 
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
@@ -89,5 +75,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (pizza.js).
+// Export the orm object for the model (burgers.js).
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,7 +13,7 @@ router.get('/', function (req, res) {
 });
 
 
-// Index Page (render all pizzas to DOM)
+// Index Page (render all burgers to DOM)
 router.get('/index', function (req, res) {
   burger.all(function(data) {
     var hbsObject = { burgers: data };
@@ -55,51 +55,5 @@ router.delete("/api/burgers/:id", function(req, res) {
 
 
 
-
-
-
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
-//   burger.all(function(data) {
-//     var hbsObject = {
-//       burgers: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-// });
-
-// //BELOW are the other routes for the insert, update, and select found in burger.js 
-
-// router.post("/api/burgers", function(req, res) {
-//   burger.insert([
-//     "burger_name", "devoured"
-//   ], [
-//     req.body.burger_name, req.body.devoured
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
-
-// router.put("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   console.log("condition", condition);
-
-//   burger.update({
-//     devoured: req.body.devoured
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-
-// Export routes for server.js to use.
 module.exports = router;
 
